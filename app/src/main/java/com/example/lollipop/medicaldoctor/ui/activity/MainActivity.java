417ed5c404ac4dd6149e762f2c702b459bba7042
R2D2 <zhuyuexx@foxmail.com>
2017-05-11 23:56:43 +0800

    新增几个fragment

--- app/src/main/java/com/example/lollipop/medicaldoctor/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/lollipop/medicaldoctor/ui/activity/MainActivity.java
@@ -1,21 +1,55 @@
 package com.example.lollipop.medicaldoctor.ui.activity;
 
 import android.os.Build;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.example.lollipop.medicaldoctor.R;
 import com.example.lollipop.medicaldoctor.app.App;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends FragmentActivity {
+
+    @BindView(R.id.frame_layout)
+    FrameLayout frameLayout;
+    @BindView(R.id.d_navigation_view)
+    NavigationView navigationView;
+    @BindView(R.id.doctor_name)
+    TextView nameText;
+    @BindView(R.id.hospital)
+    TextView hospitalText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+
+        init();
+        setStatusBar();
+    }
+
+    private void init() {
+        nameText.setText(App.getCurrentUser().getName());
+        hospitalText.setText(App.getCurrentUser().getHospital());
+
+        Menu menu = navigationView.getMenu();
+        
+    }
+
+    private void setStatusBar() {
         //当系统版本为4.4或者4.4以上时可以使用沉浸式状态栏
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             //透明状态栏
